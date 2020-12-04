b40c76809de8d27b8eece4a0558ae582c08252a1
Lucas Manzke <lmanzke@outlook.com>
2020-12-04 17:39:11 +0100

    Login hinzufügen
    Controller + Logik für Aothentication success
    Eigenen Endpoint für die API vorbereiten

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param UrlGeneratorInterface $urlGenerator
+     * @return Response
+     */
+    public function index(UrlGeneratorInterface $urlGenerator): Response {
+        return new RedirectResponse($urlGenerator->generate('app_login'));
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+             return $this->redirect('/foo');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {}
+}
