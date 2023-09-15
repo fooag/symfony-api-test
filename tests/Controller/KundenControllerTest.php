99211eff092e5b4c7fd367dafe24d33f531859b3
Felix Fluche <felix.fluche@zolar.de>
2023-09-15 12:44:55 +0200

    Add first test class

---
+++ tests/Controller/KundenControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+class KundenControllerTest extends ApiTestCase
+{
+    public function testGetCollection()
+    {
+    }
+}
