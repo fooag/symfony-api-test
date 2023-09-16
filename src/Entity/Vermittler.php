8722ddbba4577d922796f7e80c7fc0024608740e
Matthew Fedak <me@matthewfedak.co.uk>
2023-09-16 17:17:02 +0200

    Add mostly auto generated Vermittler and Vermittler Entities and Repositories

---
+++ src/Entity/Vermittler.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\VermittlerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @todo add VermittlerUser relation
+ */
+
+#[ORM\Entity(repositoryClass: VermittlerRepository::class)]
+class Vermittler
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 36)]
+    private ?string $nummer = null; // @todo field not nullable cause a generated default (see table)
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vorname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nachname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $firma = null;
+
+    #[ORM\Column]
+    private ?bool $geloescht = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNummer(): ?string
+    {
+        return $this->nummer;
+    }
+
+    public function setNummer(string $nummer): self
+    {
+        $this->nummer = $nummer;
+
+        return $this;
+    }
+
+    public function getVorname(): ?string
+    {
+        return $this->vorname;
+    }
+
+    public function setVorname(?string $vorname): self
+    {
+        $this->vorname = $vorname;
+
+        return $this;
+    }
+
+    public function getNachname(): ?string
+    {
+        return $this->nachname;
+    }
+
+    public function setNachname(?string $nachname): self
+    {
+        $this->nachname = $nachname;
+
+        return $this;
+    }
+
+    public function getFirma(): ?string
+    {
+        return $this->firma;
+    }
+
+    public function setFirma(string $firma): self
+    {
+        $this->firma = $firma;
+
+        return $this;
+    }
+
+    public function isGeloescht(): bool
+    {
+        return $this->geloescht;
+    }
+
+    public function setGeloescht(bool $geloescht): self
+    {
+        $this->geloescht = $geloescht;
+
+        return $this;
+    }
+}
---
+++ src/Entity/VermittlerUser.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\VermittlerUserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @todo add Vermittler relation
+ */
+#[ORM\Entity(repositoryClass: VermittlerUserRepository::class)]
+class VermittlerUser
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 200)]
+    private string $email;
+
+    #[ORM\Column(name: 'passwd', length: 60)]
+    private string $password; // @todo would property passwd be better?
+
+    /**
+     * @todo why has field 'aktiv' has type int in db not boolean?
+     */
+    #[ORM\Column]
+    private bool $aktiv;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isAktiv(): bool
+    {
+        return $this->aktiv;
+    }
+
+    public function setAktiv(bool $aktiv): self
+    {
+        $this->aktiv = $aktiv;
+
+        return $this;
+    }
+}
