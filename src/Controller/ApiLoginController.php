2441a77b53ec18ce793e6e182a4bd520e095feff
Matthew Fedak <me@matthewfedak.co.uk>
2023-09-16 19:57:33 +0200

    Correct routing to use foo prefix

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -7,13 +7,12 @@ namespace App\Controller;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ApiLoginController extends AbstractController
 {
-    #[Route('/api/login_check', name: 'login_check', methods: ['POST'])]
+    #[Route('/foo/login', name: 'api_login', methods: ['POST'])]
     public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager): JsonResponse
     {
         return new JsonResponse(['token' => $JWTManager->create($user)]);
